a331d10106afe45a2678e2b9de7169b170d175f6
okiwiziin <otavioamoster@gmail.com>
2022-11-07 00:03:12 -0300

    initial commit

---
+++ server/prisma/seed.ts
@@ -0,0 +1,61 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  const user = await prisma.user.create({
+    data: {
+      name: 'John Doe',
+      email: 'johndoe@gmail.com',
+      avatarUrl: 'https://github.com/okiwiziin.png'
+    }
+  })
+
+  const pool = await prisma.pool.create({
+    data: {
+      title: 'Example pool',
+      code: 'BOL123',
+      ownerId: user.id,
+
+      participants: {
+        create: {
+          userId: user.id,
+        }
+      }
+    }
+  })
+
+  const game = await prisma.game.create({
+    data: {
+      date: '2022-11-10T12:00:00.193Z',
+      firstTeamCountryCode: 'DE',
+      secondTeamCountryCode: 'BR'
+    }
+  })
+
+  await prisma.game.create({
+    data: {
+      date: '2022-11-03T12:00:00.193Z',
+      firstTeamCountryCode: 'BR',
+      secondTeamCountryCode: 'AR',
+
+      guesses: {
+        create: {
+          firstTeamPoints: 2,
+          secondTeamPoints: 1,
+
+          participant: {
+            connect: {
+              userId_poolId: {
+                userId: user.id,
+                poolId: pool.id
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+}
+
+main()
